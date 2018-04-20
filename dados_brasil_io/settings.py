3bb92da22e9da387e4f26ca1b7619245c5588a58
Mariana Bedran Lesche <maribedran@gmail.com>
2018-04-20 14:27:04 -0300

    Define Postgres como banco default

--- dados_brasil_io/settings.py
+++ dados_brasil_io/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dados_brasil_io.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brasilio',
+        'USER': 'brasilio',
+        'PASSWORD': 'brasilio',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
