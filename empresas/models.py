2fd78fbb2cabceb3c1511493f7e4f476740c81d7
Mariana Bedran Lesche <maribedran@gmail.com>
2018-05-04 15:34:32 -0300

    Remove campo de UF de empresa que não existe nos dados

--- empresas/models.py
+++ empresas/models.py
@@ -2,43 +2,8 @@ from django.db import models
 
 
 class Empresa(models.Model):
-    UNIDADES_FEDERATIVAS = (
-        ('Acre', 'AC'),
-        ('Alagoas', 'AL'),
-        ('Amapá', 'AP'),
-        ('Amazonas', 'AM'),
-        ('Bahia', 'BA'),
-        ('Ceará', 'CE'),
-        ('Distrito Federal', 'DF'),
-        ('Espírito Santo', 'ES'),
-        ('Goiás', 'GO'),
-        ('Maranhão', 'MA'),
-        ('Mato Grosso', 'MT'),
-        ('Mato Grosso do Sul', 'MS'),
-        ('Minas Gerais', 'MG'),
-        ('Paraná', 'PR'),
-        ('Paraíba', 'PB'),
-        ('Pará', 'PA'),
-        ('Pernambuco', 'PE'),
-        ('Piauí', 'PI'),
-        ('Rio Grande do Norte', 'RN'),
-        ('Rio Grande do Sul', 'RS'),
-        ('Rio de Janeiro', 'RJ'),
-        ('Rondônia', 'RO'),
-        ('Roraima', 'RR'),
-        ('Santa Catarina', 'SC'),
-        ('Sergipe', 'SE'),
-        ('São Paulo', 'SP'),
-        ('Tocantins', 'TO'),
-    )
     cnpj = models.CharField(max_length=14, primary_key=True)
     nome = models.CharField(max_length=255, null=True, db_index=True)
-    unidade_federativa = models.CharField(
-        max_length=2,
-        choices=UNIDADES_FEDERATIVAS,
-        null=True,
-        db_index=True
-    )
 
 
 class Socio(models.Model):
