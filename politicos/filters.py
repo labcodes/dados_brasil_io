cf60241f798e87f8667961924583deaf62f7350c
Mariana Bedran Lesche <maribedran@gmail.com>
2018-10-18 19:48:30 -0300

    Migra lágica de queryset da view para o filtro de deputados

--- politicos/filters.py
+++ politicos/filters.py
@@ -6,6 +6,8 @@ from politicos.models import Deputado
 class DeputadoFilter(filters.FilterSet):
     nome = filters.CharFilter(field_name='nome', lookup_expr='icontains')
     exibir_gasto_mensal = filters.BooleanFilter(method='filter_exibir_gasto_mensal')
+    gastos_mes = filters.NumberFilter(method='add_prefetch_gastos')
+    gastos_ano = filters.NumberFilter(method='add_prefetch_gastos')
 
     class Meta:
         model = Deputado
@@ -16,4 +18,13 @@ class DeputadoFilter(filters.FilterSet):
     def filter_exibir_gasto_mensal(self, queryset, name, value):
         if value and not {'gastos_mes', 'gastos_ano'} & set(self.data.keys()):
             queryset = queryset.annotate_gasto_mensal_por_deputado()
+        return queryset
+
+    def add_prefetch_gastos(self, queryset, name, value):
+        filtros = self.data.dict()
+        mes = filtros.get('gastos_mes')
+        ano = filtros.get('gastos_ano')
+        if not hasattr(self, '_prefetch_added') and mes and ano:
+            queryset = queryset.prefetch_gastos(mes=mes, ano=ano)
+            self._prefetch_added = True
         return queryset
--- politicos/views.py
+++ politicos/views.py
@@ -1,4 +1,3 @@
-from datetime import date
 from rest_framework import generics
 
 from politicos.filters import DeputadoFilter
@@ -9,19 +8,4 @@ from politicos.serializers import DeputadoSerializer
 class DeputadoListView(generics.ListAPIView):
     serializer_class = DeputadoSerializer
     filterset_class = DeputadoFilter
-
-    def get_queryset(self):
-        queryset = Deputado.objects.all().select_related('partido', 'uf')
-
-        hoje = date.today()
-        filtros = self.request.query_params.dict()
-        filtros.setdefault('gastos_mes', hoje.month)
-        filtros.setdefault('gastos_ano', hoje.year)
-
-        queryset = queryset.prefetch_gastos(**{
-            campo.replace('gastos_', ''): valor
-            for campo, valor in filtros.items()
-            if campo.startswith('gastos_')
-        })
-
-        return queryset
+    queryset = Deputado.objects.all().select_related('partido', 'uf')
