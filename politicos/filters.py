184c92466838439fd858fdbe077b5c90e5cb200c
Mariana Bedran Lesche <maribedran@gmail.com>
2018-10-18 13:27:11 -0300

    Adiciona lógica de annotate de gastos ao filtro de deputados

--- politicos/filters.py
+++ politicos/filters.py
@@ -5,9 +5,15 @@ from politicos.models import Deputado
 
 class DeputadoFilter(filters.FilterSet):
     nome = filters.CharFilter(field_name='nome', lookup_expr='icontains')
+    exibir_gasto_mensal = filters.BooleanFilter(method='filter_exibir_gasto_mensal')
 
     class Meta:
         model = Deputado
         fields = [
             'nome', 'partido', 'uf', 'id_legislatura',
         ]
+
+    def filter_exibir_gasto_mensal(self, queryset, name, value):
+        if value:
+            queryset = queryset.annotate_gasto_mensal_por_deputado()
+        return queryset
